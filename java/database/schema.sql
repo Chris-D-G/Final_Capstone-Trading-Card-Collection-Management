9197634e7ebc93fe8adb277550faf94029871ff8
Shadav Khan <shadavhkhan@gmail.com>
2023-09-25 14:12:42 -0400

    sprint 1 first draft

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,10 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS mtg_collections;
+DROP TABLE IF EXISTS mtg_cards;
+DROP TABLE IF EXISTS tcg;
 DROP TABLE IF EXISTS users;
+
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +13,29 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE tcg (
+	tgc_id SERIAL,
+	game_name varchar(30) NOT NULL UNIQUE,
+	CONSTRAINT PK_tcg PRIMARY KEY (tgc_id)
+);
+
+CREATE TABLE mtg_cards (
+	card_id int NOT NULL,
+	card_name varchar(30) NOT NULL,
+	img_url varchar(120) NOT NULL,
+	scryfall_url varchar(60) NOT NULL,
+	CONSTRAINT PK_mtg_cards PRIMARY KEY (card_id)
+);
+
+CREATE TABLE mtg_collections (
+	collection_id SERIAL,
+	card_id int NOT NULL REFERENCES mtg_cards(card_id),
+	user_id int NOT NULL REFERENCES users(user_id),
+	tgc_id int NOT NULL REFERENCES tcg(tgc_id),
+	quantity int NOT NULL,
+	CONSTRAINT PK_mtg_collections PRIMARY KEY(collection_id, card_id, user_id)
+);
+
 
 COMMIT TRANSACTION;
