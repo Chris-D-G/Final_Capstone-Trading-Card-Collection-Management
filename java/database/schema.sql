498ddfe0ab72967eb94777b574993a1f572393f9
Shadav Khan <shadavhkhan@gmail.com>
2023-09-25 15:29:08 -0400

    temporary last mockup of database schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -2,6 +2,7 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS mtg_collections;
 DROP TABLE IF EXISTS mtg_cards;
+DROP TABLE IF EXISTS user_collections;
 DROP TABLE IF EXISTS tcg;
 DROP TABLE IF EXISTS users;
 
@@ -16,8 +17,15 @@ CREATE TABLE users (
 
 CREATE TABLE tcg (
 	tgc_id SERIAL,
-	game_name varchar(30) NOT NULL UNIQUE,
+	tgc_name varchar(30) NOT NULL UNIQUE,
 	CONSTRAINT PK_tcg PRIMARY KEY (tgc_id)
+);
+
+CREATE TABLE user_collections (
+	collection_id SERIAL,
+	user_id int NOT NULL REFERENCES users(user_id),
+	tgc_id int NOT NULL REFERENCES tcg(tgc_id),
+	CONSTRAINT PK_user_collections PRIMARY KEY(collection_id)
 );
 
 CREATE TABLE mtg_cards (
@@ -28,13 +36,11 @@ CREATE TABLE mtg_cards (
 	CONSTRAINT PK_mtg_cards PRIMARY KEY (card_id)
 );
 
-CREATE TABLE card_collections (
-	collection_id int NOT NULL,
+CREATE TABLE mtg_collections (
+	collection_id int NOT NULL REFERENCES user_collections(collection_id),
 	card_id int NOT NULL REFERENCES mtg_cards(card_id),
-	user_id int NOT NULL REFERENCES users(user_id),
-	tgc_id int NOT NULL REFERENCES tcg(tgc_id),
 	quantity int NOT NULL,
-	CONSTRAINT PK_mtg_collections PRIMARY KEY(collection_id, card_id, user_id, tgc_id)
+	CONSTRAINT PK_mtg_collections PRIMARY KEY(collection_id, card_id)
 );
 
 
