f6786c6a3cce2d6f600815354783796e6a4d6d00
Christopher Guerrero <chris.d.guerrero@gmail.com>
2023-09-26 10:27:12 -0400

    updated schema to include small img url, normal img url, and updated cardDAO to include a add Card method

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -20,7 +20,8 @@ CREATE TABLE cards(
 	card_id varchar(128) NOT NULL UNIQUE, 
 	tcg_id integer NOT NULL,
 	card_title varchar(50) NOT NULL,
-	card_image_url varchar(256),
+	card_small_image_url varchar(256),
+	card_normal_image_url varchar(256),
 	card_details_url varchar(256),
 	CONSTRAINT PK_cards PRIMARY KEY(card_id),
 	CONSTRAINT FK_cards_tcg FOREIGN KEY (tcg_id) REFERENCES tcg (tcg_id)
