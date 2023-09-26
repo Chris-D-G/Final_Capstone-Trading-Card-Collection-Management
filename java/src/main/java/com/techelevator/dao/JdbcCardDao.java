7dcae2ed36330360d3fc61edfe69bd803ac61f58
Christopher Guerrero <chris.d.guerrero@gmail.com>
2023-09-25 23:11:17 -0400

    added preliminary html img components

--- java/src/main/java/com/techelevator/dao/JdbcCardDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcCardDao.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.List;
 
