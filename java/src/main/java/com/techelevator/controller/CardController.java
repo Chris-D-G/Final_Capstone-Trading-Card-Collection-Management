a3284b4859686e744e17d49204c2e2f45f87dd4b
Christopher Guerrero <chris.d.guerrero@gmail.com>
2023-09-28 17:26:40 -0400

    adding error catching and throwing to card controller

--- java/src/main/java/com/techelevator/controller/CardController.java
+++ java/src/main/java/com/techelevator/controller/CardController.java
@@ -3,10 +3,12 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.CardDao;
 import com.techelevator.model.Card;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -21,14 +23,23 @@ public class CardController{
     public CardController( CardDao cardDao){
         this.cardDao = cardDao;
     }
+
     @GetMapping(path= "/cards/search/id")
     public Card getCardByID (String searchID){
-        return cardDao.getCardById(searchID);
+       try{
+           return cardDao.getCardById(searchID);
+       }catch (RuntimeException e){
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Unable to locate card!");
+       }
     }
 
     @GetMapping(path= "/cards/search/title")
     public List<Card> getMultipleCardsByTitle (String searchTerm, @RequestParam (defaultValue= "false")  boolean isExactMatch ){
-        return cardDao.getCardsByTitle(searchTerm, isExactMatch);
+        try {
+            return cardDao.getCardsByTitle(searchTerm, isExactMatch);
+        }catch (RuntimeException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Unable to locate any cards that match!");
+        }
     }
 
 
