a81ade442136a330123c2a23444a1433885de394
Julianna Irons[B[A[B[A[B <ironsjulianna@gmail.com>
2023-09-29 13:53:33 -0400

    Changed the username display from button to routerlink/added a Profile vue page

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -10,6 +10,7 @@ import CollectionDetails from "../views/CollectionDetails.vue"
 import MyCollection from "../views/MyCollection.vue"
 import NewCollection from "../views/NewCollection.vue"
 import AddCardView from "../views/AddCardView.vue"
+import Profile from "../views/Profile.vue"
 
 Vue.use(Router)
 
@@ -96,6 +97,14 @@ const router = new Router({
       component: AddCardView,
       meta: {
         requiresAuth: true
+      }
+    },
+    {
+      path:"/profile",
+      name:"Profile",
+      component: Profile,
+      meta: {
+        requiresAuth: false
       }
     }
   ]
