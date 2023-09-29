e0dcb75bcf89aa80e7f91494bf5aee2c8a443b61
Julianna Irons[B[A[B[A[B <ironsjulianna@gmail.com>
2023-09-29 17:49:37 -0400

    Added Styling and Delete Message Update

--- vue/src/views/CollectionDetails.vue
+++ vue/src/views/CollectionDetails.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="ms-5 me-5">
-    <h1  class="text-dark fs-1 text-center fw-bold title mt-3">
-      {{this.collection.name}}
+    <h1 class="text-dark fs-1 text-center fw-bold title mt-3">
+      {{ this.collection.name }}
     </h1>
     <div class="d-flex flex-column w-25 mx-auto">
-    <button class="btn btn-dark mt-4 mb-4 " >Alphabetize</button>
-    <button class="btn btn-outline-danger btn-dark" v-on:click="deleteCollection()" >Delete Collection</button>
+      <button class="btn btn-dark mt-4 mb-4">Alphabetize</button>
+      <button
+        class="btn btn-outline-danger btn-dark mb-5"
+        v-on:click="deleteCollection(collection.name)"
+      >
+        Delete Collection
+      </button>
     </div>
     <div class="d-flex flex-wrap me-2 justify-content-evenly">
-      <card v-for="card in cards" v-bind:key="card.id" v-bind:card="card"/>
+      <card v-for="card in cards" v-bind:key="card.id" v-bind:card="card" />
     </div>
   </div>
 </template>
@@ -22,31 +27,33 @@ export default {
   data() {
     return {
       cards: [],
-      alphcards:[],
+      alphcards: [],
       collection: [],
       // search: "",
       // searched: false,
-    }  
+    };
   },
 
-  methods:{
-    changeDeck(){
-          this.cards = this.cards.toSorted();
-        },
-    deleteCollection(){
-      if(confirm("This is a permanant deletion. This will delete the Collection, all Cards associated, and your access to the Collection. There will be no reccords, and it cannot be reanimated. ARE YOU 'DEAD' SERIOUS?")){
+  methods: {
+    changeDeck() {
+      this.cards = this.cards.toSorted();
+    },
+    deleteCollection(name) {
+      if (
+        confirm(
+          "*****PERMENANT***** \nComfirmation will delete the Collection named: \n\n~ " +  name + " ~ \n\nAll Cards associated, and your access to the Collection, will be revoked.\nThere will be no reccords, and it cannot be reanimated.\n\n\nARE YOU 'DEAD' SERIOUS?"
+        )
+      ) {
         CollectionService.deleteCollection(this.$route.params.id).then(
-          response =>{
-        if(response.status == 204){
-        this.$router.push(`/myCollections`)
-             }
+          (response) => {
+            if (response.status == 204) {
+              this.$router.push(`/myCollections`);
             }
-          )
           }
-          }
-
+        );
+      }
+    },
   },
-    
 
   created() {
     CollectionService.getAllCardsByCollection(this.$route.params.id).then(
@@ -56,7 +63,7 @@ export default {
     );
 
     CollectionService.getCollectionById(this.$route.params.id).then(
-      response =>{
+      (response) => {
         this.collection = response.data;
       }
     );
@@ -72,8 +79,7 @@ export default {
 </script>
 
 <style>
-.title{
+.title {
   z-index: 1;
 }
-
 </style>
