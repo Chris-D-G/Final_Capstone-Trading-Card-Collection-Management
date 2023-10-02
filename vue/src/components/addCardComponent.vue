9f6a2673cb0c16394bcfa60bc4e75ca4f0c4d8ec
Julianna Irons[B[A[B[A[B <ironsjulianna@gmail.com>
2023-10-02 11:47:35 -0400

    Styling

--- vue/src/components/addCardComponent.vue
+++ vue/src/components/addCardComponent.vue
@@ -21,8 +21,21 @@
     <!-- To DO: add boolean check for the checkbox -->
     <label for="addToCollection"  class="fs-6 text-start me-2" > ADD </label>
     <input type="checkbox" class="form-check-input" name="addToCollection" v-model="localChecked" @change="emitChecked" >
-    
-  </div>
+
+    <div id="cardInfo" v-if="displayInfo" class="bg-primary rounded-5 border border-2 border-danger-subtle w-50 mx-auto" style="--bs-bg-opacity: .5;">
+      Card Name : {{this.addCard.name}}<br/>
+      Colors : {{this.addCard.colors}}<br/>
+      Color-Identities : {{this.addCard.colorIdentity}}<br/>
+      Set Information : {{this.addCard.setCode}} / {{this.addCard.setName}}<br/>
+      Collector # : {{this.addCard.collectorNumber}}<br/>
+      Legalities : {{this.addCard.legalities}}<br/>
+      Layout : {{this.addCard.layout}}<br/>
+      CMC : {{this.addCard.cmc}}<br/>
+      EDHREC Rank : {{this.addCard.edhrecRank}}<br/><br/>
+      Want more stats or purchase information? Click 
+      <a :href="this.addCard.scryfallUrl">Here!</a>
+    </div>
+</div>
 
   <!-- <button class="btn btn-dark p-1 btn-sm btn-outline-light ">Delete</button> -->
   </div>
@@ -43,12 +56,14 @@ export default {
     },
     displayLargerImage() {
       this.showLargeImg = !this.showLargeImg;
+      this.displayInfo=!this.displayInfo;
     },
   },
   data() {
     return {
       showLargeImg: false,
       localChecked: this.isChecked, // Initialize localChecked with the prop value
+      displayInfo:false
     
     };
   },
