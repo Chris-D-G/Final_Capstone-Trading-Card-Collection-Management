46a466f2bc9efaddb65342f55b27eb7701dfa690
Shadav Khan <shadavhkhan@gmail.com>
2023-10-02 18:14:19 -0400

    rough draft friend API

---
+++ java/src/main/java/com/techelevator/controller/UserFriendController.java
@@ -0,0 +1,47 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.UserDao;
+import com.techelevator.dao.UserFriendDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@PreAuthorize("isAuthenticated()")
+@RestController
+public class UserFriendController {
+
+    @Autowired
+    UserFriendDao userFriendDao;
+    @Autowired
+    UserDao userDao;
+
+    @RequestMapping(path = "/addFriend", method = RequestMethod.POST)
+    public int addFriend(@RequestParam int friendId, Principal principal){
+        int userId = userDao.findIdByUsername(principal.getName());
+        boolean areFriends = userFriendDao.isFriended(userId, friendId);
+        return areFriends?
+                -1:
+                userFriendDao.friendUser(userId, friendId);
+    }
+
+    @RequestMapping(path = "/unFriend", method = RequestMethod.DELETE)
+    public int deleteFriend(@RequestParam int friendId, Principal principal){
+        int userId = userDao.findIdByUsername(principal.getName());
+        boolean areFriends = userFriendDao.isFriended(userId, friendId);
+        return areFriends?
+                userFriendDao.unfriendUser(userId, friendId):
+                -1;
+    }
+
+    @RequestMapping(path = "/isFriends", method = RequestMethod.GET)
+    public boolean isFriends(@RequestParam int friendId, Principal principal){
+        int userId = userDao.findIdByUsername(principal.getName());
+        return userFriendDao.isFriended(userId, friendId);
+    }
+
+}
---
+++ java/src/main/java/com/techelevator/dao/UserFriendDao.java
@@ -0,0 +1,28 @@
+package com.techelevator.dao;
+
+public interface UserFriendDao {
+
+    /**
+     * This method checks if the user with userId is already friends with another user
+     *
+     * @param userId the sender of the friend request
+     * @param friendId the reciever of the friend request
+     * @return whether there's already a friend request
+     */
+    boolean isFriended (int userId, int friendId);
+
+    /**
+     * This method adds someone as a friend
+     * @param userId user that is sending the friend request
+     * @param friendId user that is recieving the friend request
+     */
+    int friendUser (int userId, int friendId);
+
+    /**
+     * this method removes a friend
+     * @param userId user that is removing a friend
+     * @param friendId the friend
+     */
+    int unfriendUser (int userId, int friendId);
+
+}
