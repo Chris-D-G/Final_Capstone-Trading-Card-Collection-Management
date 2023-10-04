ec1eae8e24e01c493c25ecb431a83f219c66a6fd
Amanda Schaffer <amrschaffer@gmail.com>
2023-10-03 20:00:41 -0400

    more message frontend

--- vue/src/components/MessageTable.vue
+++ vue/src/components/MessageTable.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
-      
-  </div>
+          <div>
+            <div class="d-flex">
+                <div class="d-flex flex-column border border-primary">
+                    <p class="border-bottom border-danger">Message ID </p>
+                    <p>{{messageID}}</p>
+                </div>
+        
+                <div class="d-flex flex-column border border-primary">
+                    <p class="border-bottom border-danger">Sender Name</p>
+                    <p>{{messageSender}}</p>
+                </div>
+        
+                <div class="d-flex flex-column border border-primary">
+                    <p class="border-bottom border-danger">Receiver Name</p>
+                    <p>{{messageReceiver}}</p> 
+                </div>
+        
+                <div class="d-flex flex-column border border-primary">
+                    <p class="border-bottom border-danger">TimeStamp</p>
+                    <p>{{messageTimestamp}}</p>
+                </div>
+                
+                <div class="d-flex flex-column border border-primary">
+                    <p class="border-bottom border-danger">Read Status</p>
+                    <p>{{read}}</p>
+                </div>  
+            </div>
+        </div>
 </template>
 
 <script>
