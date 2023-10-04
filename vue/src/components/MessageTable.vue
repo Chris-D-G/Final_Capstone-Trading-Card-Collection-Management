3d34c206970567221db1116856a9f13f8ea5bcf0
Christopher Guerrero <chris.d.guerrero@gmail.com>
2023-10-03 20:28:17 -0400

    implemented functional message table and added to profile

--- vue/src/components/MessageTable.vue
+++ vue/src/components/MessageTable.vue
@@ -3,35 +3,66 @@
             <div class="d-flex">
                 <div class="d-flex flex-column border border-primary">
                     <p class="border-bottom border-danger">Message ID </p>
-                    <p>{{messageID}}</p>
+                    <p
+                    v-for="(message,index) in messageList"
+                    v-bind:key="index"
+                    >{{message.messageID}}</p>
                 </div>
         
                 <div class="d-flex flex-column border border-primary">
                     <p class="border-bottom border-danger">Sender Name</p>
-                    <p>{{messageSender}}</p>
+                    <p
+                    v-for="(message,index) in messageList"
+                    v-bind:key="index"
+                    >{{message.messageSender}}</p>
                 </div>
         
                 <div class="d-flex flex-column border border-primary">
                     <p class="border-bottom border-danger">Receiver Name</p>
-                    <p>{{messageReceiver}}</p> 
+                    <p
+                    v-for="(message,index) in messageList"
+                    v-bind:key="index"
+                    >{{message.messageReceiver}}</p> 
                 </div>
         
                 <div class="d-flex flex-column border border-primary">
                     <p class="border-bottom border-danger">TimeStamp</p>
-                    <p>{{messageTimestamp}}</p>
+                    <p
+                    v-for="(message,index) in messageList"
+                    v-bind:key="index"
+                    >{{message.messageTimestamp}}</p>
                 </div>
                 
                 <div class="d-flex flex-column border border-primary">
                     <p class="border-bottom border-danger">Read Status</p>
-                    <p>{{read}}</p>
+                    <p
+                    v-for="(message,index) in messageList"
+                    v-bind:key="index"
+                    >{{message.read}}</p>
                 </div>  
             </div>
         </div>
 </template>
 
 <script>
+import MessageService from "../services/MessageService.js"
+
 export default {
-    name: "message-table"
+    name: "message-table",
+    data(){
+        return{
+            messageList: []
+        }
+    },
+    created(){
+        MessageService.getMessagesForUser()
+        .then((response)=>{
+            this.messageList = response.data;
+
+        })
+    }
+
+
 
 }
 </script>
