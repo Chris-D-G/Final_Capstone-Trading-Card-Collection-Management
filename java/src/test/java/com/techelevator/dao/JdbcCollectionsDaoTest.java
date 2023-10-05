a6ae1e77002830f26ccae957cc13d72dc4b6db53
Julianna Irons[B[A[B[A[B <ironsjulianna@gmail.com>
2023-10-05 11:27:02 -0400

    Testing

---
+++ java/src/test/java/com/techelevator/dao/JdbcCollectionsDaoTest.java
@@ -0,0 +1,90 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.CardDto;
+import com.techelevator.model.Collection;
+import com.techelevator.model.CollectionsDto;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class JdbcCollectionsDaoTest extends BaseDaoTests{
+
+    private final Collection TEST_COLLECTION1 = new Collection(400,"test1",1);
+    private final Collection TEST_COLLECTION2 = new Collection(401,"test1",1);
+    CollectionsDao cdao;
+
+    public void setup() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        cdao = new JdbcCollectionsDao(jdbcTemplate);
+    }
+
+    @Test
+    public void getAllCollections() {
+        List<CollectionsDto> actual = cdao.getAllCollections();
+        assertEquals(null,actual);
+    }
+
+    @Test
+    public void getAllUserCollections() {
+        List<Collection> actual = cdao.getAllUserCollections("username");
+        assertEquals(null,actual);
+        List<Collection> actual2 = cdao.getAllUserCollections("username2");
+        assertEquals(null,actual);
+    }
+
+    @Test
+    public void getCardsByCollectionId() {
+        List<CardDto> actual = cdao.getCardsByCollectionId(111111111);
+        assertEquals(null,actual);
+    }
+
+    @Test
+    public void addCollection() {
+       int expectedId =  cdao.addCollection(TEST_COLLECTION1,"username");
+        Collection expected =cdao.getCollectionById(expectedId);
+       assertCollectionsMatch(expected,TEST_COLLECTION1);
+    }
+
+    @Test
+    public void removeCollection() {
+        List<Collection> actual1 = cdao.getAllUserCollections("username");
+        cdao.removeCollection(11111111);
+        List<Collection> actual2 = cdao.getAllUserCollections("username");
+        assertNotEquals(actual1.size(),actual2.size());
+    }
+
+    @Test
+    public void addCardToCollection() {
+
+    }
+
+    @Test
+    public void deleteCardFromCollection() {
+    }
+
+    @Test
+    public void getCollectionById() {
+    }
+
+    @Test
+    public void getUserForCollectionId() {
+    }
+
+    @Test
+    public void getCountOfCardsInCollection() {
+    }
+
+    @Test
+    public void updateCollection() {
+    }
+
+    private void assertCollectionsMatch(Collection expected, Collection actual){
+        assertEquals( expected.getId(), actual.getId());
+        assertEquals( expected.getTcgId(), actual.getTcgId());
+        assertEquals( expected.getName(), actual.getName());
+    }
+}
