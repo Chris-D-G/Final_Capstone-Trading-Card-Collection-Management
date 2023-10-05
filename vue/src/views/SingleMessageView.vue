219bcbcc288117ac34174b094eda9adf7fc6fc4e
Amanda Schaffer <amrschaffer@gmail.com>
2023-10-05 17:16:00 -0400

    added the return to my messages button

--- vue/src/views/SingleMessageView.vue
+++ vue/src/views/SingleMessageView.vue
@@ -1,12 +1,27 @@
 <template>
-  <full-message />
+  <div>
+    <full-message />
+    <div>
+      <button
+        v-on:click.prevent="goToMyMessagse"
+        class="btn btn-success w-25 mx-auto my-2">
+          ALL MESSAGES
+      </button>
+    </div>
+    
+  </div>
 </template>
 
 <script>
 import FullMessage from "../components/FullMessage.vue"
 
 export default {
-    components: { FullMessage }
+    components: { FullMessage },
+    methods: {
+      goToMyMessagse() {
+      this.$router.push(`/messages`);
+    },
+    }
 
 }
 </script>
